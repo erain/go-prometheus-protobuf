2ea75f8032470ba1abf453e13534e006cf43a84d
Yu Yi <yiyu@google.com>
2024-07-23 15:22:17 -0400

    Revert "feat: use client library instead for reading proto"
    This reverts commit d329f6cfb5915c3ef40b40beecdd76274aff3469.

--- src/client/client.go
+++ src/client/client.go
@@ -4,62 +4,113 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 
-	"github.com/prometheus/client_model/go"
-	"github.com/prometheus/common/expfmt"
+	"github.com/golang/protobuf/proto"
+	dto "github.com/prometheus/client_model/go"
 )
 
 func main() {
-	url := "http://localhost:8080/metrics"
-	resp, err := http.Get(url)
+	// Text format
+	fmt.Println("Text format:")
+	textMetrics := getMetrics("text/plain")
+	fmt.Println(string(textMetrics))
+
+	// Binary format
+	fmt.Println("\nBinary format:")
+	binaryMetrics := getMetrics("application/vnd.google.protobuf;proto=io.prometheus.client.MetricFamily;encoding=delimited")
+	parseBinaryMetrics(binaryMetrics)
+}
+
+func getMetrics(acceptHeader string) []byte {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://localhost:8080/metrics", nil)
 	if err != nil {
-		fmt.Printf("Error fetching metrics: %v\n", err)
-		return
+		panic(err)
+	}
+	req.Header.Set("Accept", acceptHeader)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Error reading response body: %v\n", err)
-		return
+		panic(err)
 	}
 
-	metrics, err := parseBinaryMetrics(body)
-	if err != nil {
-		fmt.Printf("Error parsing metrics: %v\n", err)
-		return
-	}
+	return body
+}
 
-	for _, mf := range metrics {
-		fmt.Printf("Metric: %s\n", mf.GetName())
+func parseBinaryMetrics(data []byte) {
+	reader := bytes.NewReader(data)
+	for {
+		mf := &dto.MetricFamily{}
+		size, err := readDelimited(reader, mf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		if size == 0 {
+			break
+		}
+
+		fmt.Printf("Name: %s\n", mf.GetName())
 		fmt.Printf("Help: %s\n", mf.GetHelp())
 		fmt.Printf("Type: %s\n", mf.GetType())
+
 		for _, m := range mf.GetMetric() {
 			fmt.Printf("  Labels: %v\n", m.GetLabel())
 			if m.Gauge != nil {
-				fmt.Printf("  Gauge Value: %f\n", m.Gauge.GetValue())
+				fmt.Printf("  Gauge: %f\n", m.Gauge.GetValue())
 			}
 			if m.Counter != nil {
-				fmt.Printf("  Counter Value: %f\n", m.Counter.GetValue())
+				fmt.Printf("  Counter: %f\n", m.Counter.GetValue())
 			}
-			// Add more metric types as needed
+			// Add other metric types as needed
 		}
 		fmt.Println()
 	}
 }
 
-func parseBinaryMetrics(data []byte) ([]*io_prometheus_client.MetricFamily, error) {
-	var parser expfmt.TextParser
-	metrics, err := parser.TextToMetricFamilies(bytes.NewReader(data))
-	if err != nil {
-		return nil, err
+// The readDelimited function is designed to read Protocol Buffer messages that are encoded in a delimited format.
+// This format is used by Prometheus when sending metrics in binary form.
+//
+// Parameters:
+//   - r io.Reader: The source from which to read the delimited message.
+//   - m proto.Message: A Protocol Buffer message to unmarshal the data into.
+func readDelimited(r io.Reader, m proto.Message) (int, error) {
+	// a 1-byte buffer to read data one byte at a time.
+	buf := make([]byte, 1)
+	size := uint64(0)
+	// This loop implements varint decoding. Varints are used to encode integers using a variable number of bytes.
+	// Each byte uses 7 bits for the number and 1 bit to indicate if more bytes follow.
+	// - It reads one byte at a time.
+	// - The lower 7 bits of each byte are used to construct the size.
+	// - If the most significant bit (0x80) is set, it means another byte follows.
+	// - This continues until a byte with the most significant bit unset is found.
+	for shift := uint(0); ; shift += 7 {
+		if _, err := io.ReadFull(r, buf); err != nil {
+			return 0, err
+		}
+		b := uint64(buf[0])
+		size |= (b & 0x7F) << shift
+		if (b & 0x80) == 0 {
+			break
+		}
 	}
-
-	var result []*io_prometheus_client.MetricFamily
-	for _, mf := range metrics {
-		result = append(result, mf)
+	// After reading the size, if it's greater than 0, proceed to read the actual message:
+	if size > 0 {
+		buf = make([]byte, size)
+		if _, err := io.ReadFull(r, buf); err != nil {
+			return 0, err
+		}
+		return int(size), proto.Unmarshal(buf, m)
 	}
-
-	return result, nil
+	return 0, nil
 }
